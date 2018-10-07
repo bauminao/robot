1d04a4c846a4f187094a0dc57bd564e379431670
Baumi <baumgart.patrick+git@gmail.com>
2018-10-07 16:17:55 +0200

    Output with plot3D

--- lib/Plot.py
+++ lib/Plot.py
@@ -1,8 +1,31 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+from time import sleep
+
 
 class Plot():
     def __init__(self,model):
         self.model = model
+
+        plt.ion()
+
+        self.fig = plt.figure()
+
+        self.ax = plt.axes(projection='3d')
+        #self.ax = self.fig.add_subplot(111)
+
+        self.plot_init()
+
+
+    def plot_init(self):
+        plt.xlim(-20,20)
+        plt.ylim(-20,20)
+        plt.title('model - Nodes @ t=x.y')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+
+
 
     def plot_nodes(self):
         x = []
@@ -14,14 +37,14 @@ class Plot():
             x.append(node.x)
             y.append(node.y)
             z.append(node.z)
-        plt.xlim(-20,20)
-        plt.ylim(-20,20)
-        plt.title('model - Nodes @ t=x.y')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.scatter(x,y,s=60, c='blue', marker='x')
 
-        plt.show()
+        self.ax.plot3D(x,y,z,'r+')
+
+        self.fig.canvas.draw()
+
+        plt.waitforbuttonpress()
+        sleep(1)
+
         return True
 
     def plot_elems(self):
